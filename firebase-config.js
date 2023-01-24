df5042085df0094d57236de2481a4192c04a0b1b
julianotto <julian.kasimir.otto@gmail.com>
2023-01-24 17:27:33 +1100

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6gRs9P0B-pO12zpupaR81BsewT4kk9LM",
+  authDomain: "website-f126b.firebaseapp.com",
+  projectId: "website-f126b",
+  storageBucket: "website-f126b.appspot.com",
+  messagingSenderId: "476897576252",
+  appId: "1:476897576252:web:b81a6c0c3a80f30b808a39"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout';
+import pic from './images/1.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout sidebarSrc={pic} sidebarItems={['Home', 'About', 'Contact']}>
+      <div className="App">
+        <header className="App-header">
+          <img src={pic} className="App-logo" alt="logo" />
+          <p>
+            Suck my nuts!
+          </p>
+        </header>
+      </div>
+    </Layout>
   );
 }
 
